0c30fd87fed49b756d4cbb61b3e88fc8f20ab724
Lachezar Kutsarov <lkutsarov@gmail.com>
2025-02-05 22:20:52 +0100

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,19 @@
+
+/**
+ * @param {number[]} input
+ * @return {number}
+ */
+var maxAscendingSum = function (input) {
+    let currentSum = input[0];
+    let maxAscendingSubarraySum = input[0];
+
+    for (let i = 1; i < input.length; ++i) {
+        if (input[i - 1] >= input[i]) {
+            maxAscendingSubarraySum = Math.max(maxAscendingSubarraySum, currentSum);
+            currentSum = input[i];
+            continue;
+        }
+        currentSum += input[i];
+    }
+    return Math.max(maxAscendingSubarraySum, currentSum);
+};
